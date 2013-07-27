b6451b8f84e1c8dbd7bca4c7006e53a52aa25cfd
dannykopping <dannykopping@gmail.com>
2013-07-27 14:25:46 +0200

    Everything working now, except dates - removed int serialization and used double serialization instead for AMF3 objects

--- server.php
+++ server.php
@@ -1,10 +1,44 @@
 <?php
 require_once "vendor/autoload.php";
 
-if(function_exists('xdebug_disable'))
+//if(function_exists('xdebug_disable'))
+//{
+//    xdebug_disable();
+//}
+
+//$config = new Amfphp_Core_Config();
+//$config->serviceFolderPaths = array(dirname(__FILE__) . '/tests/');
+//$gateway = Amfphp_Core_HttpRequestGatewayFactory::createGateway($config);
+//$gateway->service();
+//$gateway->output();
+
+$data = file_get_contents("php://input");
+
+function handleError($errno, $errstr, $errfile, $errline, array $errcontext)
 {
-    xdebug_disable();
+    // error was suppressed with the @-operator
+    if (0 === error_reporting()) {
+        return false;
+    }
+
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 }
 
-set_include_path("vendor/silexlabs/amfphp/Amfphp");
-require_once "vendor/silexlabs/amfphp/Amfphp/index.php";
+set_error_handler('handleError');
+header('Content-Type: text/html;charset=utf-8');
+
+
+if($_GET['target'] == '-1956') {
+    xdebug_break();
+}
+
+try {
+    $d = amf3_decode(substr($data, 1));
+} catch (ErrorException $e) {
+    return;
+}
+//    if(strlen($php_errormsg)) {
+//        continue;
+//    }
+
+echo print_r($d, true) . "\n";
---
+++ tests/RoundtripService.php
@@ -0,0 +1,14 @@
+<?php
+
+class RoundtripService
+{
+    public function sum($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function complex($param)
+    {
+        return $param;
+    }
+}
